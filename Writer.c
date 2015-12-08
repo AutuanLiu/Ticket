c02c4ae22293042c451ccade02e6ee6937f0affe
Autuan_Liu <AutuanLiu@outlook.com>
2015-12-08 20:12:56 +0800

    add

--- Writer.c
+++ Writer.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-//Ð´Èë¿Í»§ÂòÆ±ÐÅÏ¢
+#include <stddef.h>
+#include "adt.h"
+//å†™å…¥å®¢æˆ·ä¹°ç¥¨ä¿¡æ¯
 int writefile(char *str)
 {
     FILE *fout;
     fout=fopen("guest_info.csv","ab");
-    //ÔÚÄ©Î²×·¼Ó
+    //åœ¨æœ«å°¾è¿½åŠ 
     if(fout==NULL)
         perror("guest_info.csv");
     else
@@ -13,5 +16,37 @@ int writefile(char *str)
     fclose(fout);
     return 1;
 }
-//´Ó¿Í»§ÐÅÏ¢¶ÁÈëÎÄ¼þÐÅÏ¢
+//ä»Žå®¢æˆ·ä¿¡æ¯è¯»å…¥æ–‡ä»¶ä¿¡æ¯
 
+
+void Pairl2String(Pairl p, char *s) {
+    p = p->next;
+    s[0] = '\0';
+    while(p) {
+        strcat(s, p->line_num); strcat(s, ",");
+        strcat(s, p->start_time); strcat(s, ",");
+        strcat(s, p->end_time); strcat(s, ",");
+        strcat(s, p->destination); strcat(s, ",");
+        char tmp[10];
+        sprintf(tmp, "%d", p->price);
+        strcat(s, tmp); strcat(s, ",");
+        sprintf(tmp, "%.2f", p->discount);
+        strcat(s, tmp); strcat(s, ",");
+        sprintf(tmp, "%d", p->total);
+        strcat(s, tmp); strcat(s, ",");
+        sprintf(tmp, "%d", p->left);
+        strcat(s, tmp);
+        if(p->next != NULL)
+            strcat(s, "\n");
+        p = p->next;
+    }
+}
+
+void writePairl(Pairl p) 
+{
+    char s[500];
+    Pairl2String(p, s);
+    FILE *fp = fopen("flight_info.csv","w");
+    fprintf(fp, "%s", s);
+    fclose(fp);
+}
